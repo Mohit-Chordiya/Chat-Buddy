56971fea98b0d17067b95278e22afa8c0b1943f2
Mohit-Chordiya <mohitmchordiya33@gmail.com>
2024-11-22 11:52:11 +0530

    made changes to files

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -27,7 +27,7 @@ const Main = () => {
     return (
         <div className="main">
             <div className="nav">
-                <p>Gemini</p>
+                <p>Chat Buddy</p>
                 <img src={assets.user_icon} alt="" />
             </div>
             <div className="main-container">
